25581013479438b882663cf92826049655678a08
Pierce Thompson <pierce@insprill.net>
2025-09-10 02:23:19 -0400

    Switch from grgit to git-version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,16 @@
+import com.palantir.gradle.gitversion.VersionDetails
+import groovy.lang.Closure
+
 plugins {
     kotlin("jvm") version "2.2.10"
     kotlin("plugin.serialization") version "2.2.10"
     id("com.gradleup.shadow") version "9.1.0"
     id("net.kyori.blossom") version "2.1.0"
-    id("org.ajoberstar.grgit") version "5.3.3"
+    id("com.palantir.git-version") version "4.0.0"
 }
 
 group = "net.insprill"
-version = "${project.version}+${versionMetadata()}"
+version = "${project.version}${versionMetadata()}"
 
 repositories {
     mavenCentral()
@@ -73,16 +76,18 @@ fun versionMetadata(): String {
     // CI builds only
     val buildId = System.getenv("GITHUB_RUN_NUMBER")
     if (buildId != null) {
-        return "build.${buildId}"
+        return "+build.${buildId}"
     }
 
-    val head = grgit.head()
-    var id = head.abbreviatedId
+    val versionDetails: Closure<VersionDetails> by extra
+    var id = versionDetails().gitHash
+    val git = (extra["versionDetails"] as Closure<*>)() as VersionDetails
+    var id = git.gitHash
 
     // Flag the build if the build tree is dirty
-    if (!grgit.status().isClean) {
+    if (!git.isCleanTag) {
         id += "-dirty"
     }
 
-    return "rev.${id}"
+    return "+rev.${id}"
 }
